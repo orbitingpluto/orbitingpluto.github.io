ac4057ace616d21895f06ff480a7ea4b5d01666e
Pluto Van Newkirk <mvannewkirk05@gmail.com>
2024-06-07 18:07:32 -0400

    Delete main directory

--- main/main.js
+++
@@ -1,7 +0,0 @@
-const scroll = new SmoothScroll('a[href*="#"]', {
-speed: 2000
-});
-
-window.sr = ScrollReveal();
-sr.reveal('.left-box', { reset: false }, { delay: 1000 });
-sr.reveal('.right-box', { reset: false }, { delay: 1000 });
